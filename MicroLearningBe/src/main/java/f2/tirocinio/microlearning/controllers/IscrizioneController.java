3069c01afa603347f42176bcb2c3353b3bf1ea42
elKryptos <hans_09@hotmail.it>
2024-06-17 17:48:37 +0200

    aggiunta cartella angular

--- MicroLearningBe/src/main/java/f2/tirocinio/microlearning/controllers/IscrizioneController.java
+++ MicroLearningBe/src/main/java/f2/tirocinio/microlearning/controllers/IscrizioneController.java
@@ -1,14 +1,14 @@
 package f2.tirocinio.microlearning.controllers;
 
+import f2.tirocinio.microlearning.daos.CorsoDao;
 import f2.tirocinio.microlearning.daos.IscrizioneDao;
+import f2.tirocinio.microlearning.daos.UserDao;
 import f2.tirocinio.microlearning.entities.Iscrizione;
 import f2.tirocinio.microlearning.responses.BackendResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +18,11 @@ public class IscrizioneController {
 
     @Autowired
     IscrizioneDao iDao;
+    @Autowired
+    UserDao uDao;
+    @Autowired
+    CorsoDao cDao;
+
 
     @GetMapping("/iscrizioni")
     public ResponseEntity<Object> getMyInscription(){
@@ -32,5 +37,10 @@ public class IscrizioneController {
                     "aggiunto un corso alla tua lista"));
         }
         return ResponseEntity.status(200).body(iscrizioni);
+    }
+
+    @PostMapping("/iscrizioni")
+    public ResponseEntity<Object> addIscrizione(@RequestBody Iscrizione iscrizione){
+        return null;
     }
 }
