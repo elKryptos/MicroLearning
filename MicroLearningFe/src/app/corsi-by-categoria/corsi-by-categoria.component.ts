a66b6bd92939a0cd1f31909e28c3ca310106ff10
GabrieleDibennard <gabriele.dibennardo@itsrizzoli.it>
2024-06-21 12:03:58 +0200

    ricerca categoria visualizzazione corsi

--- MicroLearningFe/src/app/corsi-by-categoria/corsi-by-categoria.component.ts
+++ MicroLearningFe/src/app/corsi-by-categoria/corsi-by-categoria.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Corsi } from '../corsi';
 import { ApiService } from '../api.service';
 import { ActivatedRoute } from '@angular/router';
@@ -13,13 +13,16 @@ export class CorsiByCategoriaComponent {
   corsi: Corsi[] = []
   categoria!: string 
 
+
   constructor(private service: ApiService, private route: ActivatedRoute ) {
     this.categoria = this.route.snapshot.paramMap.get("categoria")!;
    }
 
   ngOnInit(): void {
+    
     this.service.getInsideCategoria(this.categoria).subscribe((data) => {
       this.corsi = data
     })
   }
+
 }
